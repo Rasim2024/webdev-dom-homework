e3752d17eaf0d2b0fe0ace7dff902e9878394250
Rasim2024 <r6600666@gmail.com>
2024-05-15 08:58:34 +0300

    ДЗ 2.15 доработка 2

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-import { apiGetComments } from "./api.js";
-import { apiPostComments } from "./api.js";
-import { renderComments } from "./render.js";
 import { getComments } from "./request.js";
-import { postComments } from "./request.js";
-import { database } from "./request.js";
+import { publish } from "./button.js";
+
 
 export const nameInputElement = document.getElementById('name-input');
 export const reviewInputElement = document.getElementById('text-input');
@@ -16,31 +13,11 @@ export const loaderElement = document.getElementById('preloader')
 
 // Выводим новый комментарий из сервера на страницу
 
-getComments();
+getComments(); // сохранению в базу данных на сервер 
+
+publish();  // Кнопка публикации с защитой вода. 
 
 
-buttonElement.addEventListener("click", () => {
-    let errorInName = nameInputElement.value.length <= 2;
-    let errorInComment = reviewInputElement.value.length < 2;
-    errorInName = nameInputElement.value.trim();   //используем функцию trim(), чтобы удалить пробелы из начала и конца введенных значений. 
-    errorInComment = reviewInputElement.value.trim(); //используем функцию trim(), чтобы удалить пробелы из начала и конца введенных значений.
-
-
-    nameInputElement.style.backgroundColor = "white";
-    reviewInputElement.style.backgroundColor = "white";
-    if (errorInName === '' || errorInComment === '') {  // проверяем, не являются ли поля пустыми после удаления пробелов. Если хотя бы одно поле пустое, выводится сообщение об ошибке и запрос не отправляется.
-        nameInputElement.style.backgroundColor = "red";
-        reviewInputElement.style.backgroundColor = "red";
-        alert('Пожалуйста, заполните все поля');
-        return;
-    }
-    buttonElement.disabled = true;
-    buttonElement.textContent = 'Комментарий добавляется...';
-    // Сохранение комментариев на сервер
-    
-    postComments();
-
-});
 
 
 
