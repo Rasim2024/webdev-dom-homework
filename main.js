a1ee8922aebaa6a37b2ddd496e8da926adfb6cf9
Rasim2024 <r6600666@gmail.com>
2024-05-20 13:39:41 +0300

    2.16  Разбор

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ export const nameInputElement = document.getElementById('name-input');
 export const reviewInputElement = document.getElementById('text-input');
 export const listElement = document.getElementById('list');
 export const buttonElement = document.getElementById('button');
-export const baseURL = new URL("https://wedev-api.sky.pro/api/v1/:rasim-musaev/comments");
+export const baseURL = new URL("https://wedev-api.sky.pro/api/v2/:rasim-musaev/comments");
+export const urlApiLogin = new URL("https://wedev-api.sky.pro/api/user/login")
+
 export const loaderElement = document.getElementById('preloader')
 
 
